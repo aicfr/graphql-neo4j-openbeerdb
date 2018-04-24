f33c3f3d49a208ae956609882f5dd4c916132176
Unknown <heraud.kevin@gmail.com>
2018-04-24 17:57:45 +0200

    feat(): parse JWT token

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 import express from 'express';
+import jwt from "jsonwebtoken";
+import cors from 'cors';
 import bodyParser from 'body-parser';
 import { v1 as neo4j } from 'neo4j-driver';
 import { formatError } from 'apollo-errors';
@@ -8,11 +10,17 @@ import { merge } from 'lodash';
 import { resolvers as beerResolvers } from './resolvers/beer';
 import { resolvers as beererResolvers } from './resolvers/beerer';
 import { resolvers as breweryResolvers } from './resolvers/brewery';
-import schema from './schema/schema.graphql'
+import schema from './schema/schema.graphql';
+import dotenv from 'dotenv';
+dotenv.config()
 
 // Initialize the app
 const app = express();
 
+// enable CORS
+app.use(cors())
+
+let driver;
 const resolvers = {};
 
 // Put together a schema
@@ -21,19 +29,34 @@ const executableSchema = makeExecutableSchema({
   resolvers: merge(resolvers, beerResolvers, beererResolvers, breweryResolvers)
 });
 
-let driver;
+function getUser(authorization, secrets) {
+  const bearerLength = "Bearer ".length;
+  if (authorization && authorization.length > bearerLength) {
+    const token = authorization.slice(bearerLength);
+    console.log(token)
+    const verifiedToken = jwt.verify(token, process.env.AUTH0_SECRET)
+    console.log(verifiedToken);
+    // TODO: use sub
+  }
+}
 
 function context(headers, secrets) {
+  const authorization = headers["authorization"];
+
   if (!driver) {
     driver = neo4j.driver(
-      secrets.NEO4J_URI || 'bolt://neo4j',
+      process.env.NEO4J_URI || 'bolt://neo4j',
       neo4j.auth.basic(
-        secrets.NEO4J_USER || 'neo4j',
-        secrets.NEO4J_PASSWORD || 'openbeerdb'
+        process.env.NEO4J_USER || 'neo4j',
+        process.env.NEO4J_PASSWORD || 'openbeerdb'
       )
     );
   }
+
+  const user = getUser(authorization, secrets);
+
   return {
+    user,
     driver,
     headers
   };
@@ -42,7 +65,9 @@ function context(headers, secrets) {
 // GraphiQL, a visual editor for queries
 app.use('/graphiql', graphiqlExpress({
   endpointURL: '/graphql',
-  query: `{
+  query: `
+  // https://simple-auth0-token-creator.surge.sh/?domain=social-beer.eu.auth0.com&clientId=Wm56imkY7OWyDsoKnN1zGT6L510TB5qb
+  {
     findBeer(filter: {name: "coro"}, first: 5) {
       beerName
       abv
